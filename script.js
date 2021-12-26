4569317b569039813286eeaec3d369c411744867
moxame <moxamemoxame@gmail.com>
2021-12-26 15:13:21 +0100

    fixed some grammar

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ for (i = 0; i < 5; i++) {
     computerSelection = computerSelection.toLowerCase();
     playerSelection = prompt("Choose: ", 'Rock, Paper or Scissors')
     
-    const win = `You win ${playerSelection} beats ${computerSelection}`
-    const lose = `You lose ${computerSelection} beats ${playerSelection}`
+    const win = `You win, ${playerSelection} beats ${computerSelection}`
+    const lose = `You lose, ${computerSelection} beats ${playerSelection}`
     const draw = "It's a draw!"
     
     
     function singleRound(playerSelection,computerSelection){
         playerSelection = playerSelection.toLowerCase();
         if (playerSelection === computerSelection) return draw
-        else if (computerSelection === 'paper' &&playerSelection === 'rock') return lose
+        else if (computerSelection === 'paper' && playerSelection === 'rock') return lose
         else if (computerSelection === 'rock' && playerSelection === 'scissors') return lose
         else if (computerSelection === 'scissors' && playerSelection === 'paper') return lose
         else return win;
