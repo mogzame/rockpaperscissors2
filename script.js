d888c01f15e8c1280ff6dce0e1694a64e0620942
moxame <moxamemoxame@gmail.com>
2021-12-29 23:47:24 +0100

    added style and script to it

--- script.js
+++ script.js
@@ -1,68 +1,97 @@
+let playerScore = 0;
+let computerScore = 0;
+const rockBtn = document.getElementById('r')
+const paperBtn = document.getElementById('p')
+const scissorsBtn = document.getElementById('s')
+const playerScore_span = document.querySelector('#player-score')
+const computerScore_span = document.querySelector('#computer-score')
+const result = document.querySelector('.result')
+const leaderboard = document.getElementsByClassName('leaderboard')
+const body = document.getElementById('bod')
+const choice = document.querySelector('.choice')
+function computerPlay() {
+    const rand = Math.floor(Math.random() * 3)
+    if (rand === 0) return "Rock";
+    else if (rand === 1) return "Paper";
+    else return "Scissors";
+}
 
+computerScore_span.textContent = computerScore
 
+// replay function to be called in the endgame function
+function replay(){
 
-let playerScore = 0
-let computerScore = 0
+    const question = document.createElement('div')
+    body.appendChild(question)
 
+    question.textContent = 'Do you wanna play again?'
+    question.style.textAlign = 'center'
 
+    const answer = document.createElement('div')
+    answer.classList.add('answer')
+    body.appendChild(answer)
 
+    const yes = document.createElement('div')
+    const no = document.createElement('div')
 
-for (i = 0; i < 5; i++) {
+    yes.textContent = 'YES'
+    no.textContent = 'NO'
+    yes.classList.add('yes')
+    no.classList.add('no')
+    answer.appendChild(yes)
+    answer.appendChild(no)
+
+    yes.addEventListener('click', () => document. location. reload())
+    no.addEventListener('click', function() {
+        question.textContent = 'Maybe some other time.'
+        yes.textContent = ''
+        no.textContent = ''
+    } )
+}
+
+//function to show when the game ends
+function endGame(winner) {
+    result.textContent = `${winner} Wins!`
+    result.classList.add('resultStyle')
+    replay()
+    rockBtn.setAttribute("disabled", 1);
+    paperBtn.setAttribute("disabled", 1);
+    scissorsBtn.setAttribute("disabled", 1);
     
-    function computerPlay() {
-        const rand = Math.floor(Math.random() * 3)
-        if (rand === 0) return "Rock";
-        else if (rand === 1) return "Paper";
-        else return "Scissors";
+}
+
+function win(){
+    playerScore++;
+    playerScore_span.textContent = playerScore;
+    if (playerScore === 5) {
+        endGame('Human')
     }
-    
-    computerSelection = computerPlay()
-    computerSelection = computerSelection.toLowerCase();
-    playerSelection = prompt("Choose: ", 'Rock, Paper or Scissors')
-    
-    const win = `You win, ${playerSelection} beats ${computerSelection}`
-    const lose = `You lose, ${computerSelection} beats ${playerSelection}`
-    const draw = "It's a draw!"
-
-    function singleRound(playerSelection,computerSelection){
-        
-
-        playerSelection = playerSelection.toLowerCase();
-    
-        if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
-            if (playerSelection === computerSelection) return draw
-            else if (computerSelection === 'paper' && playerSelection === 'rock') return lose
-            else if (computerSelection === 'rock' && playerSelection === 'scissors') return lose
-            else if (computerSelection === 'scissors' && playerSelection === 'paper') return lose
-            else return win;
-        }
-
-        else return "Please enter one of the three choices."
-    
-        
 }
 
+function lose(){
+    computerScore++;
+    computerScore_span.textContent = computerScore;
+    if (computerScore === 5) {endGame('Computer')}
+}
 
-    let round = singleRound(playerSelection, computerSelection)
+function draw(){
+}
 
-    if (round === lose) {
-        computerScore++
+function singleRound(playerSelection) {
+    const computerSelection = computerPlay()
+    console.log("Player's choice is: " + playerSelection)
+    console.log("Computer's choice is: " + computerSelection)
+
+    if (playerSelection === computerSelection) return draw()
+    else if (computerSelection === 'Paper' && playerSelection === 'Rock' || computerSelection === 'Rock' && playerSelection === 'Scissors' || 
+    computerSelection === 'Scissors' && playerSelection === 'Paper') return lose()
+    else return win()
+}
+
+function game() {
+    rockBtn.addEventListener('click', () => singleRound('Rock'))
+    paperBtn.addEventListener('click', () => singleRound('Paper'))
+    scissorsBtn.addEventListener('click', () => singleRound('Scissors'))
     }
-    else if (round === win) {
-        playerScore++
-    }
-    
-     alert(singleRound(playerSelection, computerSelection))
-}
 
-let result = playerScore - computerScore
-
-if (result > 0) {
-    alert(`HUMAN WINS ${playerScore} to ${computerScore}`)
-}
-
-else if (result < 0) {
-    alert(`MACHINE BEATS HUMAN ${computerScore} to ${playerScore}`)
-}
-
-else alert("IT'S A DRAW!")
+game()
